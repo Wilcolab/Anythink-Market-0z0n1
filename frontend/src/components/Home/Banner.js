9c5923f4fa54d1283febf6d536a0f423440fc70d
edtoast <109985819+edtoast@users.noreply.github.com>
2023-01-05 00:18:10 +0000

    add frontend search

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,14 +1,36 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = ({ onSearch }) => {
+  const [title, setTitle] = React.useState("")
+  console.log(title)
+
+  React.useEffect(() => {
+    console.log(title)
+    if (title.length >= 3) {
+      onSearch(
+        title,
+        (title, page) => agent.Items.search(title, page),
+        agent.Items.search(title)
+      );
+    } else {
+      onSearch(
+        title,
+        agent.Items.all,
+        agent.Items.all()
+      );
+    }
+  }, [title])
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span>A place to </span>
-          <span id="get-part">get</span>
+          <span id="get-part">get </span>
+          <input id="search-box" size="30" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="What is it that you truly desire?" />
           <span> the cool stuff.</span>
         </div>
       </div>
