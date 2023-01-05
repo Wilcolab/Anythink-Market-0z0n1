662b4697bbabe6cb92b23f30d3d3d4c462006120
edtoast <109985819+edtoast@users.noreply.github.com>
2023-01-05 00:45:09 +0000

    add id

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -9,7 +9,7 @@ const ItemList = (props) => {
 
   if (props.items.length === 0) {
     if (props.title && props.title.length > 0) {
-      return <div className="py-4 empty">No items found for "<strong>{props.title}</strong>".</div>;
+      return <div className="py-4" id="empty">No items found for "<strong>{props.title}</strong>".</div>;
     }
     return <div className="py-4 no-items">No items are here... yet.</div>;
   }
